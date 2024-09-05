5d025480bf273b2190c9e379ac1f463b180c7a98
Kyaw Min Htway <batman066813@gmail.com>
2024-09-05 07:52:39 +0630

    conditional render

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,51 @@ import { useState } from "react";
 
 import List from "./List";
 import Item from "./Item";
+import Form from "./Form";
 
 export default function App() {
+  const [showForm, setShowForm] = useState(false);
+
   const [data, setData] = useState([
     {id:1, content: "Hello, World!", name: "Alice"},
     {id:2, content: "React is fun.", name: "Bob"},
     {id:3, content: "Yay, interesting.", name: "Chris"},
   ]);
+
   const remove = id => {
     setData(data.filter(item => item.id !== id));
   }
 
+  const add = (content, name) => {
+  const id = data.length > 0 ? data[data.length - 1].id + 1 : 1;
+  setData([...data, { id, content, name }]);
+};
+
+
   return (
     <div style={{ maxWidth: 600, margin: "20px auto" }}>
-      <h1>React</h1>
+      <h1
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}>Foodie
+         <button
+            onClick={() => setShowForm(!showForm)}
+            style={{
+              width: 32,
+              height: 32,
+              borderRadius: 50,
+              border: "0 none",
+              background: showForm ? "#dc3545" : "#0d6efd",
+              color: "white",
+            }}>
+              {showForm ? "x" : "+"}
+            </button>
+        </h1>
+
+      {showForm && <Form add={add} />}
+
       <List>
         {data.map(item => {
           return <Item key={item.id} item={item} content={item.content} name={item.name} remove={remove} />
